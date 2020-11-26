0db61cd87a45a311068ecf652bca8d79ef281762
danisdl <51409296+danisdl@users.noreply.github.com>
2020-11-25 18:48:56 -0600

    'vistas'

---
+++ vue/src/views/Contactos.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <div>
+<div class="row justify-content-around ">
+        <div class="col-5 ml-5">
+            <h5 class="text-center">Por favor introduzaca algun numero y su nombre</h5>
+            <div class="">
+                <label>Nombre</label>
+                <input type="text" class=" col-12 form-control">
+            </div>
+            <div class="">
+                <label>Telefono</label>
+                <input type="text" class=" col-12 form-control">
+            </div>
+            <div class="row justify-content-around m-2">
+            <button class="col-4 btn btn-outline-dark">¡Ok!</button>
+            <button class="col-4 btn btn-outline-dark">¡Mostrar tus contactos!</button>
+            </div>
+        </div>
+</div>
+</div>
+        <div>
+            <h5>Sus contactos son: </h5>
+                <table class="table">
+                    <thead >
+                        <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Telefono</th>
+                        </tr>
+                    </thead>
+                    <tbody >
+                        <tr>
+                        <th scope="row">Daniela solis de leon</th>
+                        <td>6181001479</td>                      
+                        </tr>
+                    </tbody>
+                </table>
+        </div>
+
+</div>
+</template>
